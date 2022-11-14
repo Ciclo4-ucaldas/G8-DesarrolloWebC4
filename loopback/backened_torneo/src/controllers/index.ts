954f6534328bbd42393068111133ed2af7517a0c
thunderthard <hars1302@gmail.com>
2022-11-14 11:30:21 -0500

    creacion controller arbitro

--- loopback/backened_torneo/src/controllers/index.ts
+++ loopback/backened_torneo/src/controllers/index.ts
@@ -4,3 +4,4 @@ export * from './torneo-equipo.controller';
 export * from './equipo-torneo.controller';
 export * from './torneo-estadio.controller';
 export * from './estadio.controller';
+export * from './arbitro.controller';
